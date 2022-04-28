3f9ba3e78582c0a0f95a7bd8443fe4efddc092c0
Ana Zitzow <anasempai89@gmail.com>
2022-04-28 10:03:08 -0700

    commit tree 929ddeef63fb6f9fcb49b05b89ab5b628a357fa0

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution here
+const findMatching = (array, str) => array.filter(drivers => drivers.startsWith(str) || drivers.toLowerCase() === str.toLowerCase());
+
+const fuzzyMatch = (names, str) => names.filter(name => name.startsWith(str));
+
+const matchName = (driver, str) => driver.filter(name => name.name === str);
+
